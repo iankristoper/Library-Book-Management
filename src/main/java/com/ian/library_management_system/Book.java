10f7784ab43984373af3ca0ccdcab5db43cb522c
iankristoper <iankristoper26@gmail.com>
2024-03-16 10:01:14 +0800

    UPDATE: added book class features

--- src/main/java/com/ian/library_management_system/Book.java
+++ src/main/java/com/ian/library_management_system/Book.java
@@ -10,6 +10,55 @@ package com.ian.library_management_system;
 
 public class Book 
 {
+    //private attributes for the book
     private String title;
-    private 
+    private String author;
+    private String ISBN;
+    private boolean isAvailable;
+    
+    
+    //CONSTRUCTOR method -> used to initialize value when new object is created 
+    public Book(String title, String author)
+    {
+        this.title = title;
+        this.author = author;
+        this.isAvailable = true;
+    }
+    
+    
+    //GETTER methods -> used to return value from the private attirbutes
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public String getAuthor()
+    {
+        return author; 
+    }
+    
+    public boolean isAvailable()
+    {
+        return isAvailable;
+    }
+    
+    
+    //SETTER methods -> used to set value to the private attributes 
+    public void setAvailable(boolean available)
+    {
+        isAvailable = available;
+    }
+    
+    @Override 
+    public String toString()
+    {
+        return "Book{" + "title='" + title + '\'' + ", author='" + author + '\'' + ", isAvailable=" + isAvailable + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
 }
