a88c991e6920ce3c6737761317f591af0caf7334
iankristoper <iankristoper26@gmail.com>
2024-03-16 10:24:12 +0800

    UPDATE: added library class features

---
+++ src/main/java/com/ian/library_management_system/Library.java
@@ -0,0 +1,30 @@
+package com.ian.library_management_system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ *
+ * @author Ian Tagano
+ */
+
+
+public class Library 
+{
+    private List<Book> books;
+    
+    public Library()
+    {
+        this.books = new ArrayList<>();
+    }
+    
+    
+    public void addBook(Book book)
+    {
+        books.add(book);
+    }
+    
+    public void 
+    
+}
