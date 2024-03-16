c3b2c1b192d0fe384a4034903b552ae43e71b51b
iankristoper <iankristoper26@gmail.com>
2024-03-16 11:17:05 +0800

    UPDATE: added library class features

--- src/main/java/com/ian/library_management_system/Library.java
+++ src/main/java/com/ian/library_management_system/Library.java
@@ -25,6 +25,47 @@ public class Library
         books.add(book);
     }
     
-    public void 
+    public void displayAvailableBooks()
+    {
+        System.out.println("Available books: ");
+        
+        for(Book book : books)
+        {
+            if(book.isAvailable())
+            {
+                System.out.println(book);
+            }
+        }
+    }
+    
+    public void lendBook(String title)
+    {
+        for(Book book : books)
+        {
+            if(book.getTitle().equalsIgnoreCase(title) && book.isAvailable())
+            {
+                book.setAvailable(false);
+                System.out.println("You have borrowed: " + book.getTitle());
+                return;
+            }
+        }
+        
+        System.out.println("Sorry, the book is not available for borrowing");
+    }
+    
+    public void returnBook(String title)
+    {
+        for(Book book : books)
+        {
+            if(book.getTitle().equalsIgnoreCase(title) && !book.isAvailable())
+            {
+                book.setAvailable(true);
+                System.out.println("You have return " + book.getTitle());
+                return;
+            }
+        }
+        
+        System.out.println("This is not valid book to return");
+    }
     
 }
